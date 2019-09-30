49cab968ff5f18e29d92579252bcb1f6a233490c
ragnariock <39524490+ragnariock@users.noreply.github.com>
2019-09-30 17:44:12 +0300

    Update classify_nsfw.py

--- classify_nsfw.py
+++ classify_nsfw.py
@@ -103,7 +103,7 @@ def main(argv):
     args = parser.parse_args()
     #image_data = open(args.input_file).read()
     with open(args.input_file, 'rb') as f:
-        image_data = BytesIO()
+        #image_data = BytesIO()
         image_data.write(f.read)
         #image_data = f.read()
     # Pre-load caffe model.
